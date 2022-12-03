98650db8635542abb3f882396350979c76a8c93d
Eugene Hlushak <hlushak.e@gmail.com>
2022-12-03 11:56:06 +0200

    add edit

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,8 @@
 const inputUserName = document.querySelector("#name-input");
 const outputUserName = document.querySelector("#name-output");
 
-inputUserName.addEventListener("input", () => {
-  outputUserName.textContent = inputUserName.value;
+inputUserName.addEventListener("input", (event) => {
+  return event.currentTarget.value
+    ? (outputUserName.textContent = event.currentTarget.value)
+    : (outputUserName.textContent = "Anonymous");
 });
